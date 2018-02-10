7d579594e4edee59783fff70d2bdbc56d5edc667
WangJinTao418 <wangjintao76114987@qq.com>
2018-02-10 10:14:25 +0800

    第一次提交

---
+++ src/main/java/cn/wjt/git/Two.java
@@ -0,0 +1,8 @@
+package cn.wjt.git;
+
+public class Two {
+	public static void main(String[] args) {
+		System.out.println("第二次上传到testGit!");
+	}
+
+}
---
+++ src/main/java/cn/wjt/git/testGit.java
@@ -0,0 +1,8 @@
+package cn.wjt.git;
+
+public class testGit {
+	public static void main(String[] args) {
+		System.out.println("第一次上传到testGit！");
+	}
+
+}
